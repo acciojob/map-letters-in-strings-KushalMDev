37f87c788c58cc30893428a2b15ab83acd2ebd6f
KushalMDev <105456987+KushalMDev@users.noreply.github.com>
2024-03-25 17:15:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+function mapLetters(s){
+    let obj={}
+    let i=0;
+    for(let c of s){
+        if(obj[c]===undefined){
+            obj[c]=[i]
+        }
+        else{
+            obj[c].push(i)
+        }
+        i++
+    }
+    return obj
+}
